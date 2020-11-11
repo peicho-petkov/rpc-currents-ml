0d8a676985d1f2da171e49808e895a91638bc8f3
peicho petkov <peicho.petkov@gmail.com>
2020-11-11 09:08:05 +0200

    change code formating

--- db_tools/db_tables.py
+++ db_tools/db_tables.py
@@ -2,13 +2,17 @@ class dbTable:
     def __init__(self,tablename):
         self.tablename=str(tablename)
         self.colls={}
+    
     def add_coll(self,colname,coltype):
         self.colls[str(colname)]=str(coltype)
+    
     def del_coll(self,colname):
         del self.colls[str(colname)]
+    
     def get_myqsl_create_query(self):
         query='CREATE TABLE {} ('.format(self.tablename)
         for colname, coltype in self.colls.items():
             query = "{} {} {},".format(query,colname,coltype)
         query=query+')'
         return query
+    
