f0b5ef5a797742af9063eeef0ffe7263c656d4b7
Peicho Petkov <Peicho.Petkov@cern.ch>
2020-12-14 22:27:50 +0100

    tables added

--- Trainer/DataManager.py
+++ Trainer/DataManager.py
@@ -150,8 +150,9 @@ class DataPopulator:
             dpid_col=dpid_col_name, ichange_col=ichange_col_name, imon_col=imon_col_name, vmon_col=vmon_col_name,flag_col=flag_col_name)
         self._dbcon._cursor.executemany(query,rpccurr_data)
 
-    def update_env_parameters(self,rpccurr_table,ichange_col_name,uxc_press_col_name,uxc_temp_col_name,uxc_rh_col_name,uxc_dp_colname,uxc_data):
-        query="UPDATE {table} SET {press_col}={press}, {temp_col}={temp}, {rh_col}={rh}, {dp_col}={dp} WHERE {press_col}==NULL and {temp_col}==NULL and {rh_col}==NULL and {ichagne_col}<={uxc_change_time}".format(table=rpccurr_table, ichange_col=ichange_col_name,press_col=uxc_press_col_name,temp_col=uxc_temp_col_name,rh_col=uxc_rh_col_name,dp_col=uxc_dp_colname,uxc_change_time=uxc_data[0],press=uxc_data[1],temp=uxc_data[2],rh=uxc_data[3],dp=uxc_data[4])
+    def update_env_parameters(self, uxc_table, uxc_press_col_name,uxc_temp_col_name,uxc_rh_col_name,change_date_col,until_col_name,uxc_data,until_timestamp):
+#        query="UPDATE {table} SET {press_col}={press}, {temp_col}={temp}, {rh_col}={rh}, {dp_col}={dp} WHERE {press_col} is NULL and {temp_col} is NULL and {rh_col} is NULL and {ichange_col} BETWEEN '{uxc_change_time}' and '{intil}'".format(table=rpccurr_table, ichange_col=ichange_col_name,press_col=uxc_press_col_name,temp_col=uxc_temp_col_name,rh_col=uxc_rh_col_name,dp_col=uxc_dp_colname,uxc_change_time=uxc_data[0],press=uxc_data[1],temp=uxc_data[2],rh=uxc_data[3],dp=uxc_data[4])
+        query="INSERT INTO {table} ({press_col}, {temp_col}, {rh_col}, {change_date_col}, {until_col}) VALUES ('{press}', '{temp}', '{rh}', '{change_date}','{until}')".format(table=uxc_table, change_date_col=change_date_col,press_col=uxc_press_col_name,temp_col=uxc_temp_col_name,rh_col=uxc_rh_col_name,until_col=until_col_name,change_date=uxc_data[0],press=uxc_data[1],temp=uxc_data[2],rh=uxc_data[3],until=until_timestamp)
         self._dbcon.execute_query_self(query)
 
 
@@ -205,7 +206,7 @@ def fill_imon_vmon_data():
 
     flag=56
 
-    dpids=open("/afs/cern.ch/user/p/ppetkov/work/public/dpids")
+    dpids=open("/afs/cern.ch/user/p/ppetkov/work/public/dpids-sample")
     
     for dpid in dpids:
         dpid=dpid.strip()
@@ -218,16 +219,12 @@ def fill_imon_vmon_data():
 
             print("dpid ",dpid, "start date ", ce._startdate," enddate ", ce._enddate)
             
-            # rpcdata = ce.get_rpccurrents_data("cms_rpc_pvss_test.RPCCURRENTS",dpid,flag,["DPID","CHANGE_DATE","IMON","VMON","FLAG"])
-            # dp.insert_imon_many(rpccurr_table="TrainingData",dpid_col_name="DPID",ichange_col_name="CHANGE_DATE", imon_col_name="IMON",vmon_col_name="VMON",flag_col_name="FLAG",rpccurr_data=rpcdata)
-            # dp.commit_inserted_records()
-            
             for rpc_data in ce.get_rpccurrents_data("cms_rpc_pvss_test.RPCCURRENTS",dpid,flag,["DPID","CHANGE_DATE","IMON","VMON","FLAG"]):
                 dp.insert_imon_record(rpccurr_table="TrainingData",dpid_col_name="DPID",ichange_col_name="CHANGE_DATE", imon_col_name="IMON",vmon_col_name="VMON",flag_col_name="FLAG",rpccurr_data=rpc_data)
   
             dp.commit_inserted_records()
 
-if __name__ == "__main__":
+def update_uxc_data():
     print("stating...")
     omds = oracle_dbConnector(user='cms_rpc_test_r',password='rpcr20d3R')
     omds.connect_to_db('cman_int2r')
@@ -248,36 +245,27 @@ if __name__ == "__main__":
     edate=datetime.datetime(2018,12,12)
 
     flag=56
-
-    dpids=open("/afs/cern.ch/user/p/ppetkov/work/public/dpids")
-    
-    for dpid in dpids:
-        dpid=dpid.strip()
-
-        fromdate=sdate
-        while fromdate<edate: 
-            todate=fromdate+relativedelta(months=1)
-            ce.set_time_widow(fromdate,todate)
-            fromdate=todate
-
-            print("dpid ",dpid, "start date ", ce._startdate," enddate ", ce._enddate)
-            
-            # rpcdata = ce.get_rpccurrents_data("cms_rpc_pvss_test.RPCCURRENTS",dpid,flag,["DPID","CHANGE_DATE","IMON","VMON","FLAG"])
-            # dp.insert_imon_many(rpccurr_table="TrainingData",dpid_col_name="DPID",ichange_col_name="CHANGE_DATE", imon_col_name="IMON",vmon_col_name="VMON",flag_col_name="FLAG",rpccurr_data=rpcdata)
-            # dp.commit_inserted_records()
-            
-            for uxc_data in ce.get_uxc_env_data("cms_rpc_pvss_test.UXC_ENVIRONMENT",["CHANGE_DATE","PRESSURE","TEMPERATURE","RELATIVE_HUMIDITY","DEWPOINT"]):
-                dp.insert_imon_record(rpccurr_table="TrainingData",ichange_col_name='CHANGE_DATE',uxc_press_col_name='uxcPressure',uxc_temp_col_name='uxcTemperature',uxc_rh_col_name='uxcRH',uxc_dp_colname='uxcDPoint',uxc_data)
-  
-            dp.commit_inserted_records()
-
-    # ce.set_time_widow(datetime.datetime(2018,10,10),datetime.datetime(2018,11,3))
-    # print(ce._startdate)
-    # print(ce._enddate)
-    # ce.set_flag_col_name("FLAG")
-    # ce.set_timestamp_col_name("CHANGE_DATE")
-    # ce.set_dpid_col_name("DPID")
-    # ce.get_rpccurrents_data("cms_rpc_pvss_test.RPCCURRENTS",315,56,["DPID","CHANGE_DATE","IMON","VMON","FLAG"])
-    # ce.get_uxc_env_data("cms_rpc_pvss_test.UXC_ENVIRONMENT",["CHANGE_DATE","PRESSURE","TEMPERATURE","RELATIVE_HUMIDITY","DEWPOINT"])
+    fromdate=sdate
+    old_data=[]
+    while fromdate<edate: 
+        todate=fromdate+relativedelta(days=1)
+        ce.set_time_widow(fromdate,todate)
+        fromdate=todate
+        
+        print("start date ", ce._startdate," enddate ", ce._enddate)
+        
+        for uxc_data in ce.get_uxc_env_data("cms_rpc_pvss_test.UXC_ENVIRONMENT",["CHANGE_DATE","PRESSURE","TEMPERATURE","RELATIVE_HUMIDITY","DEWPOINT"]):
+            if old_data[1:4]!=uxc_data[1:4] and len(old_data)==5:
+                print("sending to db: different old ",old_data," new ", uxc_data)
+                dp.update_env_parameters(uxc_table="UXC_ENV",uxc_press_col_name='uxcPressure',uxc_temp_col_name='uxcTemperature',uxc_rh_col_name='uxcRH',change_date_col='CHANGE_DATE',until_col_name='NEXT_CHANGE_DATE',uxc_data=old_data,until_timestamp=uxc_data[0])
+                old_data=uxc_data[:]
+            if(len(old_data)<5):
+                old_data=uxc_data[:]
+        
+        dp.commit_inserted_records()
 
     print("done...")
+
+if __name__=='__main__':
+#    fill_imon_vmon_data()
+#    update_uxc_data()
--- db_tools/db_tables.py
+++ db_tools/db_tables.py
@@ -27,7 +27,131 @@ class dbTable:
             if "not null" in str(coltype).to_lower():
                 collnames.append(conname)
         return collnames
+
+class TrainigDataTable(dbTable):
+    def __init__(self,tablename='TrainigData'):
+        super().__init__(tablename)
+        self.add_call("rec_id","bigint auto_increment primary key")
+        self.add_coll("LAST_UPDATE","timestamp not null")
+        self.set_change_date_col()
+        self.set_iom_col()
+        self.set_vom_col()
+        self.set_pdid_col()
+        self.set_flag_col()
+        self.set_inst_lumi_col()
+        self.set_uxc_p()
+        self.set_uxc_t()
+        self.set_uxc_rh()
+        self.set_uxc_dp()
+        self.set_integrated_lumi()
+        self.set_hours_without_lumi()
+        
+        
+    def set_iom_col(self,name='IMON',type="float not null"):
+        self.imon=name
+        self.add_coll(name,type)
+
+    def set_vom_col(self,name='VMON',type="float not null"):
+        self.vmon=name
+        self.add_coll(name,type)
     
+    def set_pdid_col(self,name='DPID',type="mediumint not null"):
+        self.dpid=name
+        self.add_coll(name,type)
+
+    def set_change_date_col(self,name="CHANGE_DATE",type="timestamp not null"):
+        self.change_date=name
+        self.add_coll(name,type)
+
+    def set_flag_col(self,name="FLAG",type="smallint not null"):
+        self.flag=name
+        self.add_coll(name,type)
+
+    def set_inst_lumi_col(self,name="InstLumi",type="loat default null"):
+        self.instant_lumi=name
+        self.add_coll(name,type)
+    
+    def set_uxc_p(self,name="uxcPressure",type="float default null"):
+        self.uxcP=name
+        self.add_coll(name,type)
+
+    def set_uxc_t(self,name="uxcTemperature",type="float default null"):
+        self.uxcT=name
+        self.add_coll(name,type)
+
+    def set_uxc_rh(self,name="uxcRH",type="float default null"):
+        self.uxcRH=name
+        self.add_coll(name,type)
+	    
+    def set_uxc_dp(self,name="uxcDPoint",type="float default null"):
+        self.uxcDP=name
+        self.add_coll(name,type)
+
+    def set_integrated_lumi(self,name="IntegratedLumi",type="float default null"):
+        self.interated_lumi=name
+        self.add_coll(name,type)
+
+    def set_hours_without_lumi(self,name="HoursWithoutLumi",type="float default null"):
+        self.hours_without_lumi=name
+        self.add_coll(name,type)
+
+class LumiDataTable(dbTable):
+    def __init__(self, tablename):
+        super().__init__(tablename)
+        self.add_call("rec_id","bigint auto_increment primary key")
+        self.add_coll("LAST_UPDATE","timestamp not null")
+        self.set_ls_start_col()
+        self.set_ls_stop_col()
+        self.set_inst_lumi()
+        self.set_integrated_lumi()
+
+    def set_ls_start_col(name="STARTTIME",type="TIMESTAMP not null"):
+        self.ls_start=name
+        self.add_coll(name,type)
+
+    def set_ls_stop_col(name="STOPTIME",type="TIMESTAMP not null"):
+        self.ls_stop=name
+        self.add_coll(name,type) 
+
+    def set_inst_lumi(name="INSTLUMI",type="FLOAT not null"):
+        self.inst_lumi=name
+        self.add_coll(name,type)
+
+    def set_integrated_lumi(name="INTEGRATED",type="FLOAT default null"):
+        self.integrate_lumi=name
+        self.add_coll(name,type)
+
+class UxcEnvTable(dbTable):
+    def __init__(self, tablename):
+        super().__init__(tablename) 
+        self.add_call("rec_id","bigint auto_increment primary key")
+        self.add_coll("LAST_UPDATE","timestamp not null")  
+        self.set_change_date_col()
+        self.set_next_change_date_col()
+        self.set_pressure()
+        self.set_temperature()
+        self.set_relative_humidity()
+
+   def set_change_date_col(self,name="CHANGE_DATE",type="timestamp not null"):
+        self.change_date=name
+        self.add_coll(name,type)
+
+   def set_next_change_date_col(self,name="NEXT_CHANGE_DATE",type="timestamp not null"):
+        self.next_change_date=name
+        self.add_coll(name,type)
+
+    def set_pressure(self,name="uxcPressure",type="float default null"):
+        self.pressure=name
+        self.add_coll(name,type)
+
+    def set_temperature(self,name="uxcTemperature",type="float default null"):
+        self.temperature=name
+        self.add_coll(name,type)
+
+    def set_relative_humidity(self,name="uxcRH",type="float default null"):
+        self.relative_humidity=name
+        self.add_coll(name,type) 
+
 from base import mysql_dbConnector
 
 if __name__ == "__main__":
@@ -64,3 +188,4 @@ if __name__ == "__main__":
     print(TrainingData.get_myqsl_create_query())
     print(Lumidata.get_myqsl_create_query())
 
+#CREATE TABLE UXC_ENV ( rec_id bigint auto_increment primary key, LAST_UPDATE timestamp not null, CHANGE_DATE timestamp not null, NEXT_CHANGE_DATE timestamp not null, uxcPressure float default null, uxcTemperature float default null, uxcRH float default null);
