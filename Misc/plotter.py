b56d3dcc38fecbe72f124c60f9539d6204851523
EltonSh <shumka.toni@gmail.com>
2021-03-24 19:05:47 +0200

    new plot class

--- Misc/plotter.py
+++ Misc/plotter.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt 
 import pandas as pd 
+import numpy as np
 
 class simple_plotter:
     ''' Plots versus date, i.e. assumes time as x-axis 
@@ -21,4 +22,38 @@ class simple_plotter:
         if filename == "":
             plt.show()
         else:
-            plt.savefig(filename)
+            plt.savefig(filename) 
+
+class simple_plotter_opt:
+    ''' Plots versus date, i.e. assumes time as x-axis 
+    '''
+    
+    def __init__(self, columns, data, time_format='%Y-%m-%d %H:%M:%S'):
+        self.load_data(columns, data, time_format)
+
+    def load_data(self, columns, data, time_format='%Y-%m-%d %H:%M:%S'):
+        self.columns = list(columns.values()) 
+        ncols = len(self.columns)
+        nrows = len(data)
+        xcolumn = str(columns[0])
+        self.date = np.zeros(nrows, dtype=np.float)
+        self.pred = np.zeros(nrows, dtype=np.float)
+        self.meas = np.zeros(nrpws, dtype=np.float)
+        i = 0 
+        for row in data:
+            self.date[i] = matplotlib.dates.date2num(row[0])
+            self.pred[i] = row[1]
+            self.meas[i] = row[2]
+            i = i + 1
+        
+    def plot_it(self, xlabel="Date [YYYY-mm-dd]", ylabel="Current [uA]", filename=""):
+        plt.gcf().autofmt_xdate() 
+        plt.ylabel("Current [uA]") 
+        plt.plot(self.date,self.pred,'.-',label="predicted", alpha=0.5)
+        plt.plot(self.date,self.meas,'.-',label="measured",alpha=0.5) 
+        plt.legend()
+        if filename == "":
+            plt.show()
+        else:
+            plt.savefig(filename) 
+
