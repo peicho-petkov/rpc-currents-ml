c68fc77473db7daf9909b7735d6e43f70f35694a
EltonSh <shumka.toni@gmail.com>
2021-03-24 20:41:30 +0200

    New Method called

--- plot_compare_currents.py
+++ plot_compare_currents.py
@@ -10,8 +10,12 @@ from Misc import plotter
 
 if __name__ == '__main__':
     oparser = OptionParser()
-    oparser.add_option("--model-id", action="store", type="int", dest="model_id", 
+    oparser.add_option("--model-id", action="store", type="int", dest="model_id", default=-1,
                         help="The model_id of the model used to make the prediction, integer")
+    oparser.add_option("--modelconf-name", action="store", type="string", dest="conf_name",
+                        default="", help="Select model by model configuration name")
+    oparser.add_option("--dpid", action="store", type="int", dest="DPID",
+                        default=-1, help="Select model by dpid")
     oparser.add_option("--pred-start-date", action="store", type="string", dest="start_date",\
                         help="The beginning of the prediction period you want to plot [yyyy-mm-dd]")
     oparser.add_option("--pred-finish-date", action="store", type="string", dest="finish_date", 
@@ -22,18 +26,33 @@ if __name__ == '__main__':
     (options, args) = oparser.parse_args()
 
     model_id = options.model_id
+    modelconf_name = options.conf_name
+    dpid = options.DPID 
     start_date = datetime.strptime(options.start_date, '%Y-%m-%d')
     finish_date = datetime.strptime(options.finish_date, '%Y-%m-%d')
     filename = options.filename
+
+    if dpid < 0 and modelconf_name == "" and model_id < 0:
+        print("To chose a model, specify --dpid and --modelconf_id or --model_id!")
+    
     rpccurrml = dbase.mysql_dbConnector(host='rpccurdevml',user='ppetkov',password='cmsrpc')
     rpccurrml.connect_to_db('RPCCURRML')
+
+    if model_id < 0:
+        query = f" select MLModels.model_id from MLModels, MLModelsConf where MLModels.MODELCONF_ID=MLModelsConf.modelconf_id and MLModelsConf.NAME='{modelconf_name}'"
+        model_id = rpccurrml.fetchall_for_query_self(query)[0][0]
 
     extractor_pred_curr_table = DataManager.Extractor_MySql(table_predicted_current.tablename, rpccurrml)
     extractor_pred_curr_table.set_column_name_list(["predicted_for", "predicted_value", "measured_value"])
     extractor_pred_curr_table.set_timestamp_col('predicted_for')
     extractor_pred_curr_table.set_time_widow(start_date, finish_date)
+    extractor_pred_curr_table.set_model_id_col_name()
+    extractor_pred_curr_table.set_model_id(model_id)
+    extractor_pred_curr_table.set_dpid_col_name('dpid')
+    extractor_pred_curr_table.set_DPID(dpid)
+   
 
-    query = extractor_pred_curr_table.get_data_query()
+    query = extractor_pred_curr_table.get_data_by_model_id_query()
     data = rpccurrml.fetchall_for_query_self(query)
 
     plotter = plotter.simple_plotter({0:"predicted_for",1:"predicted_value",2:"measured_value"},data)
