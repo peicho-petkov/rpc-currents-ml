4468d81e967269f0e912dececa8135708d31da81
EltonSh <shumka.toni@gmail.com>
2021-03-24 16:37:43 +0100

    Tested the plot script

--- Misc/plotter.py
+++ Misc/plotter.py
@@ -9,13 +9,14 @@ class simple_plotter:
         self.load_data(columns, data, time_format)
 
     def load_data(self, columns, data, time_format='%Y-%m-%d %H:%M:%S'):
-        self.columns = columns 
+        self.columns = list(columns.values()) 
+        xcolumn = str(columns[0])
         self.dataframe = pd.DataFrame( [[ij for ij in i] for i in data] )
         self.dataframe.rename(columns=columns, inplace=True)
-        self.dataframe.columns.items[0] = pd.to_datetime(self.dataframe[columns.items[0]], format=time_format) 
+        self.dataframe[xcolumn] = pd.to_datetime(self.dataframe[xcolumn], format=time_format) 
 
     def plot_it(self, xlabel="Date [YYYY-mm-dd]", ylabel="Current [uA]", filename=None):
-        self.dataframe.set_index([self.columns.items[0]], inplace=True)
+        self.dataframe.set_index([self.columns[0]], inplace=True)
         self.dataframe.plot(legend=True, xlabel=xlabel, ylabel=ylabel, use_index=True)
         if filename is None:
             plt.show()
