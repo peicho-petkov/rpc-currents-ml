b79217998461c57a6fb40c90856c08d1025a1f8f
EltonSh <shumka.toni@gmail.com>
2021-04-08 15:32:24 +0200

    test notifications script tested

--- test_notifications_module.py
+++ test_notifications_module.py
@@ -1,14 +1,14 @@
-from db_tools import table_notifications
+from db_tools import table_notifications, table_predicted_current
 from NotificationModule import NotificationManager
 from db_tools import rpccurrml
-from db_tools import base 
+from db_tools import base as dbase
 
 
-rpccurrml = mysql_dbConnector(host='rpccurdevml',user='ppetkov',password='cmsrpc')
+rpccurrml = dbase.mysql_dbConnector(host='rpccurdevml',user='ppetkov',password='cmsrpc')
 rpccurrml.connect_to_db('RPCCURRML')
 rpccurrml.self_cursor_mode()
 
-notmanager = NotificationManager(rpccurrml,100)
+notmanager = NotificationManager.NotificationManager(rpccurrml,table_predicted_current,100)
 
 notificationstable=table_notifications
 query=notificationstable.get_myqsl_create_query()
