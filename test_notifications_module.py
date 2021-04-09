b8f6e1c11f9ae924cc4f28913bdf88ca15a66317
EltonSh <shumka.toni@gmail.com>
2021-04-10 01:25:04 +0300

    Script to extract, analyse data and write warnings

--- test_notifications_module.py
+++ test_notifications_module.py
@@ -8,14 +8,22 @@ rpccurrml = dbase.mysql_dbConnector(host='rpccurdevml',user='ppetkov',password='
 rpccurrml.connect_to_db('RPCCURRML')
 rpccurrml.self_cursor_mode()
 
+
+extractor_pred_curr_table = DataManager.Extractor_MySql(table_predicted_current.tablename, rpccurrml)
+extractor_pred_curr_table.set_column_name_list(["predicted_for", "predicted_value", "measured_value"])
+extractor_pred_curr_table.set_timestamp_col('predicted_for')
+extractor_pred_curr_table.set_dpid_col_name('dpid')
+extractor_pred_curr_table.set_DPID(317)
+extractor_pred_curr_table.set_model_id_col_name('model_id')
+extractor_pred_curr_table.set_model_id(4)
+
+query=extractor_pred_curr_table.get_data_by_model_id_query()
+data=rpccurrml.fetchall_for_query_self(query)
+
 notmanager = NotificationManager.NotificationManager(rpccurrml,table_predicted_current,100)
+notmanager.load_data(data)
+notmanager.analyse()
+for message, timestamp, avgdiff in analyse():
+    query=table_notifications.get_insert_notification_query(317,4,message,timestamp,avgdiff,1,0,0)
 
-notificationstable=table_notifications
-
-#query=notificationstable.get_myqsl_create_query()
-#rpccurrml.execute_commit_query_self(query)
-
-query=notificationstable.get_insert_notification_query(316,23,'error','2017-06-06',6.5,1,0,0)
-query1=notificationstable.get_update_acknowledged_col_query(1,316,23)
-rpccurrml.execute_commit_query_self(query1)
 
