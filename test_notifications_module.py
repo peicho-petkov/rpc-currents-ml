ec984879cfd6083336945a3dc180ad7d4ce29005
EltonSh <shumka.toni@gmail.com>
2021-04-10 11:14:54 +0200

    Test notifications script update

--- test_notifications_module.py
+++ test_notifications_module.py
@@ -1,8 +1,9 @@
 from db_tools import table_notifications, table_predicted_current
 from NotificationModule import NotificationManager
+from TrainerModule import DataManager
 from db_tools import rpccurrml
 from db_tools import base as dbase
-
+from datetime import datetime
 
 rpccurrml = dbase.mysql_dbConnector(host='rpccurdevml',user='ppetkov',password='cmsrpc')
 rpccurrml.connect_to_db('RPCCURRML')
@@ -16,14 +17,21 @@ extractor_pred_curr_table.set_dpid_col_name('dpid')
 extractor_pred_curr_table.set_DPID(317)
 extractor_pred_curr_table.set_model_id_col_name('model_id')
 extractor_pred_curr_table.set_model_id(4)
+start_date=datetime.strptime('2016-05-06','%Y-%m-%d')
+end_date=datetime.strptime('2016-06-30','%Y-%m-%d')
+extractor_pred_curr_table.set_time_widow(start_date,end_date)
 
 query=extractor_pred_curr_table.get_data_by_model_id_query()
 data=rpccurrml.fetchall_for_query_self(query)
 
 notmanager = NotificationManager.NotificationManager(rpccurrml,table_predicted_current,100)
+notmanager.set_soft_limit()
+notmanager.set_hard_limit()
+notmanager.set_persistence_time()
 notmanager.load_data(data)
-notmanager.analyse()
-for message, timestamp, avgdiff in analyse():
-    query=table_notifications.get_insert_notification_query(317,4,message,timestamp,avgdiff,1,0,0)
-
+# notmanager.analyse()
+for message, timestamp, avgdiff in notmanager.analyse():
+    
+    myquery=table_notifications.get_insert_notification_query(317,4,message,timestamp,avgdiff,1,0,0)
+    rpccurml.execute_commit_query_self(myquery)
 
