4dc9509e5adfe627005e5f279392e3f51b81f5fb
EltonSh <shumka.toni@gmail.com>
2021-04-15 18:14:54 +0300

    Same as cl tool, but now a method

---
+++ train_hv_channel_method.py
@@ -0,0 +1,15 @@
+import RPCHVChannelModel
+import h2o
+
+def train(model_conf_name, dpid, flag=56, mojopath, modelpath):
+    conf_name = model_conf_name
+    
+    h2o.init()
+    
+    RPCHVChannelModel.init(model_conf_name=conf_name,mojofiles_path=mojopath,mlmodels_path=modelpath)
+    
+    model_id = RPCHVChannelModel.train_and_register_for_dpid(dpid,flag,True)
+    if model_id < 0:
+        print(f"a model configuration with name {conf_name} already registered for DPID {dpid}...")
+    else:
+        print(f"An ML model with model_id {model_id} with configuration name {conf_name} for DPID {dpid} was registered successfully...")
