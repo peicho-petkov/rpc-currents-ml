7c3eb2ff14fc10e666b36a910d5378fd130778d3
EltonSh <shumka.toni@gmail.com>
2021-04-15 18:14:33 +0300

    Sets models to active

---
+++ set_active_models_for_confname.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from TrainerModule import MLModelManager
+from db_tools import table_mlmodels, table_mlmodelsconf
+from optparse import OptionParser
+
+if __name__ == "__main__":
+    oparser = OptionParser()
+    oparser.add_option("--modelconf-name", action="store", type="string", dest="modelconf_name",
+                        help="The modelconfname of the models you want to train")
+
+    (options, args) = oparser.parse_args()
+    modelconf_name = options.modelconf_name
+
+    rpccurrml = dbase.mysql_dbConnector(host="rpccurdevml", user="ppetkov", password="cmsrpc")
+    rpccurrml.connect_to_db("RPCCURRML")
+    rpccurrml.self_cursor_mode()
+
+    modelmanager = MLModelManager.MLModelsManager(rpccurrml, table_mlmodels)
+    modelmanager.set_mlmodels_active_state(table_mlmodelsconf, modelconf_name)
