b3201a924b851293149122bc0189c94227a7335c
EltonSh <shumka.toni@gmail.com>
2021-06-21 21:49:20 +0200

    plot script added

--- test_fill_training_data_table.py
+++ test_fill_training_data_table.py
@@ -6,26 +6,26 @@ import csv
 import sys
 # DataManager.fill_imon_vmon_uxc_data_for_DPIDs([420])
 
-# print("Start: ",sys.argv[1])
-# print("Stop: ",sys.argv[2])
-# fromDpid = int(sys.argv[1])
-# toDpid   = int(sys.argv[2])
+print("Start: ",sys.argv[1])
+print("Stop: ",sys.argv[2])
+fromDpid = int(sys.argv[1])
+toDpid   = int(sys.argv[2])
 
-# inpfile = open("/afs/cern.ch/user/e/eshumka/rpc-currents-ml/new_dpids.csv", "r")
-# thedpids = inpfile.readlines()
-# dpids = []
+inpfile = open("/afs/cern.ch/user/e/eshumka/rpc-currents-ml/new_dpids.csv", "r")
+thedpids = inpfile.readlines()
+dpids = []
 
-# for ent in thedpids:
-#     ent = ent.replace("\n", "")
-#     dpids.append(ent)
+for ent in thedpids:
+    ent = ent.replace("\n", "")
+    dpids.append(ent)
 
-# dpids = list( dict.fromkeys(dpids) )
-# dpids.sort(reverse=False, key=int)
-# dpids = dpids[fromDpid:toDpid]
+dpids = list( dict.fromkeys(dpids) )
+dpids.sort(reverse=False, key=int)
+dpids = dpids[fromDpid:toDpid]
 # print(dpids)
 # print(len(dpids))
-dpids = ['316']
-startdate = datetime(2016,5,5)
+# dpids = ['364']
+startdate = datetime(2016,5,1)
 enddate = datetime(2018,12,3)
 ftt = fill_new_data.FillTrainingTable(rpccurrml, omds, dpids, startdate, enddate)
 #ftt.fill_inst_lumi_table(time_step = 1)  # time step in months
