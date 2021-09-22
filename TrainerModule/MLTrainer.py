58c4eb60690bac73ef0498ffc9d0ee8e51d572b5
Peicho Petkov <peicho.stoev.petkov@cern.ch>
2021-09-22 16:55:51 +0300

    autoenc v2 added

--- TrainerModule/MLTrainer.py
+++ TrainerModule/MLTrainer.py
@@ -201,9 +201,15 @@ class MLTrainer:
         print(self.model_conf.input_cols)
         print(n_inputs)
         rpc_ae = RPCAutoencoder(n_inputs=n_inputs)
-        rpc_ae.set_layers_one_and_five_size(768)
-        rpc_ae.set_layers_two_and_four_size(256)
-        rpc_ae.set_central_layer_size(64)
+        if self.model_conf.mlclass == "AUTOENC_V1":
+            rpc_ae.set_layers_one_and_five_size(512)
+            rpc_ae.set_layers_two_and_four_size(128)
+            rpc_ae.set_central_layer_size(64)
+        elif self.model_conf.mlclass == "AUTOENC_V2":
+            rpc_ae.set_layers_one_and_five_size(768)
+            rpc_ae.set_layers_two_and_four_size(256)
+            rpc_ae.set_central_layer_size(64)
+            
         rpc_ae.create_network()
         ae_dm = AE_DataManager()
         
